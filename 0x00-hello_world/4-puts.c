0422bee99d8a4dd7123a0952a2fd6465abfb538a
Assioua <zakaria.assioua@outlook.fr>
2023-07-13 14:28:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main code
- * return : always 0
+ * return : always null
  */
 int main(void)
 {
- puts ("\"Programming is like building a multilingual puzzle");
- return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
