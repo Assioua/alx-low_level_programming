fe4bc8d0882dcda64572965b31329b10bfe494a2
Assioua <zakaria.assioua@outlook.fr>
2023-07-14 10:58:53 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		puts("is zero");
+		printf("%d is zero \n" , n); 
 	}
 	if (n > 0)
 	{
-		puts("is positive");
+		printf("%d is positive \n" , n);
 	}
 	if (n < 0)
 	{
-		puts("is negative");
+		printf("%d is negative \n" , n);
 	}
 	return (0);
 }
