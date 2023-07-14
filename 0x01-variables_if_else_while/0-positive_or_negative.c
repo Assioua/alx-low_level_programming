0e7621a569a93940d6e8efafff9f8b7e392b9d77
Assioua <zakaria.assioua@outlook.fr>
2023-07-14 10:45:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,22 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* Main : calculating if n is equal to zero , negative or positive */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        if(n==0)
+	if (n == 0)
 	{
 		puts("is zero");
 	}
-	if(n>0)
+	if (n > 0)
 	{
 		puts("is positive");
 	}
-	if(n<0)
+	if (n < 0)
 	{
 		puts("is negative");
 	}
