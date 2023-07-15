166d38828357a620c0721e38493b349d1e544050
Assioua <zakaria.assioua@outlook.fr>
2023-07-15 13:44:26 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all letters except q and e
+ * Return: return success
+*/
+
+int main(void)
+{
+	char ch;
+
+	for ( ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if ( ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
