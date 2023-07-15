6d33eafd0be8d5e875bcd5d395719cc8138e59c5
Assioua <zakaria.assioua@outlook.fr>
2023-07-15 13:49:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all digits
+ * Return: return success
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		printf("%d" , i);
+	}
+	putchar('\n');
+
+	return (0);
+}
