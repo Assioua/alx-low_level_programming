4625c82fc6a6e00506c95645ecf3a7ddf0152e4f
Assioua <zakaria.assioua@outlook.fr>
2023-07-16 03:04:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 16
+ * Return: return sucess
+*/
+
+int main(void)
+{
+	char j;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar('0' + i);
+	}
+
+	for (j = 'a' ; j <= f ; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
