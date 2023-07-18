2ddbb471398fd114940355d8df9cbb856614746d
Assioua <zakaria.assioua@outlook.fr>
2023-07-18 15:32:42 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints all hours and minutes
+*/
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 23 ; i++)
+	{
+		if (i < 10)
+		{
+			_putchar('0' + 0 + i);
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+
+		for (j = 0 ; j <= 59 ; j++)
+		{
+			if (j < 10)
+			{
+				_putchar('0' + 0 + j);
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
+		}
+	}
+}
