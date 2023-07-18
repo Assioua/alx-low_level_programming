e4423b719e0fba19b737c7a9a8ef83e7241a572a
Assioua <zakaria.assioua@outlook.fr>
2023-07-18 15:35:49 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,19 @@ void jack_bauer(void)
 
 	for (i = 0 ; i <= 23 ; i++)
 	{
+		
+
+		for (j = 0 ; j <= 59 ; j++)
+
 		if (i < 10)
 		{
 			_putchar('0' + 0 + i);
 		}
+
 		else
 		{
 			_putchar('0' + i);
 		}
-
-		for (j = 0 ; j <= 59 ; j++)
-		{
 			if (j < 10)
 			{
 				_putchar('0' + 0 + j);
