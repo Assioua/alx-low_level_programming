934a85663ed1577a9861847a5eb20ca537ede10e
Assioua <zakaria.assioua@outlook.fr>
2023-07-20 12:20:44 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,18 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, firstNbr, lastNbr;
 
 	for (i = 0 ; i < 14 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar('0' + j/10);
-				_putchar('0' + j%10);
+			{	
+				firstNbr = j/10;
+				lastNbr = j%10;
+				_putchar('0' + firstNbr);
+				_putchar('0' + lastNbr);
 			}
 
 			if (j < 10)
