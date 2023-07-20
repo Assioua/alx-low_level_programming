ed22b395119467f02a25f8c16a28cd202e045cb1
Assioua <zakaria.assioua@outlook.fr>
2023-07-20 11:37:21 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if param is digit
+ * @c: param passed by user
+ * Return: 1 if digit 0 if not
+*/
+
+int _isdigit(int c)
+{
+	int i, rs;
+
+	rs = 0;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i == c)
+		{
+			rs = 1;
+		}
+	}
+
+	return (rs);
+}
