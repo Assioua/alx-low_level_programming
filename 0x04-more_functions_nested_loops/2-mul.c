8af4e7d734c8cbe42aae0394a5b5f171108c9c7b
Assioua <zakaria.assioua@outlook.fr>
2023-07-20 11:46:15 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - return multiplication of two numbers
+ * @a: param 1
+ * @b: param 2
+ * Return: return the results of a*b
+*/
+
+int mul(int a, int b)
+{
+	int rs;
+
+	rs = a * b;
+
+	return (rs);
+}
