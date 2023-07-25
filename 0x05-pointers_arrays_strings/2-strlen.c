75c582a4434f61d4c9f4a76b73dfe06ea442681e
Assioua <zakaria.assioua@outlook.fr>
2023-07-25 10:31:38 -0700

    size of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * int _strlen - returns the length of a string
+ *  _strlen - returns the length of a string
  * @s: param
  * Return: returns the numbers of caracters in a string
 */
