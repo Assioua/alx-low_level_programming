ee981cb18a39de801fad872ccd353f2782339a3f
Assioua <zakaria.assioua@outlook.fr>
2023-07-25 09:28:10 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * void swap_int - swap two integers
+ * @a: first param int
+ * @b: second int param
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	a* = *b;
+	*b = c;
+}
