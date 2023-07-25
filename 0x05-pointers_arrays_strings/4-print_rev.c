30f77193d9792543fee2931ea3f98d02c40394ba
Assioua <zakaria.assioua@outlook.fr>
2023-07-25 14:26:45 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_dev - print sentence in reverse
+ * @s: param
+*/
+
+void print_rev(char *s)
+{
+	int a = 0;
+	int b = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for ( b = a ; b >= 0 ; b--)
+	{
+		_putchar(s[b]);
+	}
+
+	_putchar('\n');
+}
