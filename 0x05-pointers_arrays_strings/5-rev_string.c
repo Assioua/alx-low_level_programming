082b427fbe734b0ab358b35926197a18093d2a89
Assioua <zakaria.assioua@outlook.fr>
2023-07-25 14:33:18 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: param
+*/
+
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (b = a ; b >= a ; b--)
+	{
+		if (s[b] != '\0')
+		{
+			_putchar(s[b]);
+		}
+	}
+}
