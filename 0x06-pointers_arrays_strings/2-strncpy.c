9c68c6d43e25da29053b6538a7565b355c2c4df5
Assioua <zakaria.assioua@outlook.fr>
2023-07-26 10:47:35 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copie a string to another
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes
+ * Return: return the new string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
