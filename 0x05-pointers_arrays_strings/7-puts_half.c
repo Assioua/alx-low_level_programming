a11fde9117d4036f4a7e77f3a83d0ee30b338672
Assioua <zakaria.assioua@outlook.fr>
2023-07-25 17:45:17 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int a, b, i;
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	if (a % 2 == 0)
+	{
+		for (i = a / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (a % 2)
+	{
+		for (b = (a - 1) / 2; n < b - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
