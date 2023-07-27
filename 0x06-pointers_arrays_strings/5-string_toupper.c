5eb837c0559c6d3dda18cdb3b5645528df5925f1
Assioua <zakaria.assioua@outlook.fr>
2023-07-27 10:29:11 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,31 +5,15 @@
  * @str: param
  * Return: returns the array with all letters in upper case
 */
-
 char *string_toupper(char *str)
 {
-        int i = 0;
-        int j;
-        char x;
+	int i;
 
-        while (str[i] != '\0')
-        {
-                i++;
-        }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
 
-        for (j = 0; j < i; j++)
-        {
-                for (x = 'a'; x <= 'z'; x++)
-                {
-                        if (str[j] == x)
-                        {
-                                str[j] = x - 32;
-                        }
-
-                }
-
-        }
-
-        return (str);
-
+	return (str);
 }
