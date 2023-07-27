a0f751768d9837743ca243d4de9fc57ed75eef1a
Assioua <zakaria.assioua@outlook.fr>
2023-07-27 11:43:58 -0700

    capiutalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all first letter
+ * @str: param
+ * Return: new capitalized string
+*/
+
+char *cap_string(char *str)
+{
+        int i, j;
+        char arr_sep[] = {32,9,10,44,59,46,33,63,34,40,41,123,125};
+
+        for (i = 0; str[i] != '\0' ; i++)
+        {
+                if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+                {
+                        str[i] -= 32;
+                }
+
+                for (j = 0; j < 13; j++)
+                {
+                        if (arr_sep[j] == str[i] && str[i+1] >= 'a' && str[i+1] <= 'z')
+                        {
+                                str[i+1] -= 32;
+                        }
+                }
+
+        }
+
+        return (str);
+
+}
