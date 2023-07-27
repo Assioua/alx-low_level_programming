4d127c63c0c87e97c4a6ca6a0979fba8a709e51d
Assioua <zakaria.assioua@outlook.fr>
2023-07-27 04:41:48 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+	int i;
+
+	for (i = 0; s1[i] && s2[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
 }
