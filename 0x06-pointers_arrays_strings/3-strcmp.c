5bae22e329dc7e6bf3580af2188d462f3b773f01
Assioua <zakaria.assioua@outlook.fr>
2023-07-27 04:38:39 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,20 @@
  * _strcmp - compare two string
  * @s1: first param
  * @s2: second param
+ *
  * Return: returns 0 if strings are equal , if not a positive or negative number
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	while (s1 == s2)
 	{
-		if (*s1 == '\0')
+		if (s1 == '\0')
 		{
 			return (0);
 		}
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (s1 - s2);
 }
