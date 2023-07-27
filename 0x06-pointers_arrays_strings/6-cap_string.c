f43fc9dd75f75e9537990b7ce50ab8d27ae61689
Assioua <zakaria.assioua@outlook.fr>
2023-07-27 12:01:47 -0700

    capiutalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,26 @@
 
 char *cap_string(char *str)
 {
-        int i, j;
-	
-        char arr_sep[] = {32,9,10,44,59,46,33,63,34,40,41,123,125};
+	int i, j;
 
-        for (i = 0; str[i] != '\0' ; i++)
-        {
-                if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-                {
-                        str[i] -= 32;
-                }
-                for (j = 0; j < 13; j++)
-                {
-                        if (arr_sep[j] == str[i] && str[i+1] >= 'a' && str[i+1] <= 'z')
-                        {
-                                str[i+1] -= 32;
-                        }
-                }
-        }
-        return (str);
+	char arr_sep[] = {32,9,10,44,59,46,33,63,34,40,41,123,125};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == arr_sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
 }
