be9c4bc0b2c77a8a358dca4807254e869fe486a5
Assioua <zakaria.assioua@outlook.fr>
2023-07-31 13:56:49 -0700

    search string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,25 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-	unsigned int count = 1;
+	unsigned int count = 0, i, j;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (j = 0; s[j] != '\0'; j++)
-	{
-	if (accept[i] == s[j])
-	{
-	count++;
-	break;
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++;
+			}
+		}
+		else
+			return (count);
 	}
+		return (count);
 
-	}
-	if (i == count - 1)
-	{
-	break;
-	}
-
-	}
-	return (count);
 }
