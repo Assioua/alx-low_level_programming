a942d1eda41e8208847c3980e612ea9c7047c515
Assioua <zakaria.assioua@outlook.fr>
2023-07-31 13:33:19 -0700

    search string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *  _strchr - search in string
+ *  @s: string param
+ *  @c: char to search
+ *
+ *  Return: return pointer to string match
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i ;
+	char *p = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+
+	}
+	return (p);
+}
