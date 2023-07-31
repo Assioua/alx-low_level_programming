6a56e6cd6b179c2bdaf174388f5595ed25af16c2
Assioua <zakaria.assioua@outlook.fr>
2023-07-31 13:07:33 -0700

    copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  _memcpy - copy string to string
+ *  @dest: first param
+ *  @src: second param
+ *  @n: number of bytes to copy
+ *
+ *  Return: return new string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0' && dest[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
