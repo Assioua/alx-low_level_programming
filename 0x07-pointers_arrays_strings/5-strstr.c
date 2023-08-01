6494fc327bcd43aa117d9f0a39dd1fab742e27df
Assioua <zakaria.assioua@outlook.fr>
2023-08-01 12:45:06 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strstr - return substring if found in first string
+ * @haystack: first string
+ * @needle: substring
+ *
+ * Return: returns substring if found if not return null
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	char *p = NULL;
+
+	while (haystack[i] != '\0')
+	{
+	j = 0;
+	if (haystack[i] == needle[j])
+	{
+	p = &haystack[i];
+	for (j = 1; needle[j] != '\0'; j++)
+	{
+	if (haystack[i + j] != needle[j])
+	{
+	printf("test \n");
+	p = NULL;
+	break;
+	}
+	}
+	}
+	i++;
+
+	if (p != NULL)
+	{
+	break;
+	}
+	}
+	return (p);
+}
