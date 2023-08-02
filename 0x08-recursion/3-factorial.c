01338b4aa0ce17509e2a16348f128d2fe394e8e9
Assioua <zakaria.assioua@outlook.fr>
2023-08-02 12:10:51 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - calculate facotrial of a number
+ * @n: number param
+ *
+ * Return: returns factorial of n
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+}
