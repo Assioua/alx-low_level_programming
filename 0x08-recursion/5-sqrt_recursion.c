e81f253afbc7151075e1aa4466b852e1de163b11
Assioua <zakaria.assioua@outlook.fr>
2023-08-03 12:29:22 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check_for_square - checks for the square root
+ * @x:first param
+ * @y:second param
+ *
+ * Return: int
+ */
+int check_for_square(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	if (x * x > y)
+		return (-1);
+	return (check(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: first param
+ *
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check_for_square(1, n));
+}
