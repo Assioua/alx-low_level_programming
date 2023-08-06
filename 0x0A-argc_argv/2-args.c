1b9c414c15b26c83ac7f40e2eee23ae88f915a6c
Assioua <zakaria.assioua@outlook.fr>
2023-08-06 12:40:20 -0700

    arg print

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - returns arguments passed to main
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: return sucess
+*/
+
+int main(int argc, char *argv)
+{
+	int i = 0;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
