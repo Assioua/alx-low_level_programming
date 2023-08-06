df1c0c311be3ccef2810c00b92c7ebe9597e9e40
Assioua <zakaria.assioua@outlook.fr>
2023-08-06 12:42:47 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,13 @@
  * Return: return sucess
 */
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	int i = 0;
 
 	for (i = 0 ; i < argc ; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", *argv++);
 	}
 
 	return (0);
