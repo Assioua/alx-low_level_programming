6f9058295244c693dca59d4db903cb6cac651b0b
Assioua <zakaria.assioua@outlook.fr>
2023-08-14 16:53:56 -0700

    first strcut

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dog define struct
+ * @name: first param
+ * @age: second param
+ * @owner: third param
+ *
+ * Description: struct of a dog
+*/
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
