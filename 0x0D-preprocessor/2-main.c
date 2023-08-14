0138286c89a7abdedcaa96ffc1bc758f420062ca
Assioua <zakaria.assioua@outlook.fr>
2023-08-14 05:32:25 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include <stdio.h>
 
 /**
  * main - print file of execution
@@ -6,14 +6,8 @@
 
 int main(void)
 {
-	char arr[] = __FILE__;
-	int i;
+	printf("%s", __FILE__);
+	printf('\n');
 
-	for (i = 0 ; arr[i] != '\0' ; i++)
-	{
-		_putchar(arr[i]);
-	}
-	
-	_putchar('\n');
 	return (0);	
 }
