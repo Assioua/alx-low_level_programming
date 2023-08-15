32d6f72114a5bb2d71634196aa0f9c1ab7183b37
Assioua <zakaria.assioua@outlook.fr>
2023-08-15 13:45:17 -0700

    free strcut

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,7 +3,17 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+ * free_dog - frees dog
+ * @d: pointer to dog
+ */
+
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
