e58f6b1cbb86a15ecc7b4c246cd1d280d451a735
Assioua <zakaria.assioua@outlook.fr>
2023-08-15 04:53:15 -0700

    first typdedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
