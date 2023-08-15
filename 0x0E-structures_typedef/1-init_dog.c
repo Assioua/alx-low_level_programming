cd0ca11ec9782437c12411534087999b9737cc6b
Assioua <zakaria.assioua@outlook.fr>
2023-08-14 17:41:17 -0700

    function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *dg;
-
-	dg = malloc(sizeof(struct dog));
-	dg = d;
-	dg->name = name;
-	dg->age = age;
-	dg->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
